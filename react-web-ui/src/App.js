68bff2774885498127427b236168eb450e75321f
Emircan Dalman <emircandalman28@gmail.com>
2021-07-03 15:35:15 +0300

    Initial commit

---
+++ react-web-ui/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Home from './Home/Home'
+import Header from './Header/Header'
+import Footer from './Footer/Footer'
+import SearchPage from './SearchPage/SearchPage'
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="app">
+      <Router>
+        <Header />
+        
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        
+        <Footer />
+      </ Router>
+    </div>
+  );
+}
+
+export default App;
